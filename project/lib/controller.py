68d9157c50607ec78f7e2601546200dd96f7602b
Michael.L <126206742+MLiondis@users.noreply.github.com>
2025-08-14 10:52:01 +1000

    state machinery

--- project/lib/controller.py
+++ project/lib/controller.py
@@ -1,32 +1,72 @@
-from movement import Movement
-from victim_sensor import Victim_Sensor
-from PiicoDev_VL53L1X import PiicoDev_VL53L1X
-from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
-from servo import Servo
-from machine import Pin, PWM
-from time import sleep
+from time import sleep, time
 
-range_front = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-range_left = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+class Controller: 
+    def __init__(self, wheels, Lultra, Fultra, colour_sensor, debug):
+        self.__wheels = wheels
+        self.__Lultra = Lultra
+        self.__Fultra = Fultra
+        self.__colour = colour_sensor
+        self.state = "IDLE"
+        self.last_state_change = time()
+        self.__debug = debug
 
-freq = 50
-min_us = 500
-max_us = 2500
-dead_zone_us = 1500
+        def idle_state(self): #     STOP state
+            if self.__debug:
+                print("System: IDLE state")
+            self.state = "IDLE"
+            self.__wheels.stop()
 
-servo_pwm_left = PWM(Pin(16))
-servo_pwm_right = PWM(Pin(15))
+        def set_read_dist(self):
+            if self.__debug:
+                print("System: READ state")
+            self.state = "READ"
+            print(Lultra.distance_mm, Fultra.distance_mm)
 
-left_servo = Servo(pwm=servo_pwm_left, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq)
+        def set_move_forwards_state(self):
+            if self.__debug:
+                print("System: FORWARDS state")
+            self.state = "FORWARDS"
+            self.__wheels.move_forward()
 
-right_servo = Servo(pwm=servo_pwm_right, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq)
+        def set_move_backwards_state(self):
+            if self.__debug:
+                print("System: BACKWARDS state")
+            self.state = "BACKWARDS"
+            self.__wheels.move_backward()
 
-movement = Movement(right_servo, left_servo, False)
+        def set_rotate_right_state(self):
+            if self.__debug:
+                print("System: RIGHT state")
+            self.state = "RIGHT"
+            self.__wheels.rotate_right()
 
-while True:
-    print(range_front.distance_mm, range_left.distance_mm)
-    sleep(0.1)
-    if range_left.distance_mm <= 100:
-        movement.move_forward()
-    elif range_left.distance_mm >= 100:
-        movement.stop()
+        def set_rotate_left_state(self):
+            if self.__debug:
+                print("System: LEFT state")
+            self.state = "LEFT"
+            self.__wheels.rotate_left()
+
+        def error_state(self):
+            if self.__debug:
+                print("System: ERROR state")
+            self.state = "ERROR"
+            self.__wheels.stop() 
+
+        def update(self):
+            time_now = time.now()
+            if self.state == "IDLE":
+                self.idle_state()
+                if self.__last_state_change - time_now >= 5:
+                    self.set_forwards_state()
+            elif self.state == "READ":
+                self.set_read_dist()
+            elif self.state == "FORWARDS":
+                self.set_move_forwards_state()
+            elif self.state == "BACKWARDS":
+                self.set_move_backwards_state()
+            elif self.state == "RIGHT":
+                self.set_rotate_right_state()
+            elif self.state == "LEFT":
+                self.set_rotate_left_state()
+            else:
+                self.error_state()
---
+++ project/py_scripts/controller_unittest.py
@@ -0,0 +1,65 @@
+from controller import Controller  
+from movement import Movement
+from victim_sensor import Victim_Sensor
+from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
+from PiicoDev_VEML6040 import PiicoDev_VEML6040
+from PiicoDev_SSD1306 import *
+from servo import Servo
+from machine import Pin, PWM
+from time import sleep
+
+range_front = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
+range_left = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+
+sensor = PiicoDev_VEML6040()
+display = create_PiicoDev_SSD1306()
+
+freq = 50
+min_us = 500
+max_us = 2500
+dead_zone_us = 1500
+
+servo_pwm_left = PWM(Pin(16))
+servo_pwm_right = PWM(Pin(15))
+
+left_servo = Servo(pwm=servo_pwm_left, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq)
+
+right_servo = Servo(pwm=servo_pwm_right, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq)
+
+movement = Movement(right_servo, left_servo, False)
+
+system = Controller(movement, range_left, range_front, sensor, display, True)
+
+while True: 
+    print("testing system")
+    sleep(2)
+    print("testing forwards state")
+    system.set_move_forwards_state()
+    sleep(2)
+    print("testing idle state")
+    system.idle_state()
+    sleep(2)
+    print("testing backwards state")
+    system.set_move_backwards_state()
+    sleep(2)
+    print("testing lturn state")
+    system.set_rotate_left_state()
+    sleep(2)
+    print("testing rturn state")
+    system.set_rotate_right_state()
+    sleep(2)
+    system.set_idle_state()
+
+
+
+
+
+
+
+# while True:
+#     print(range_front.distance_mm, range_left.distance_mm)
+#     sleep(0.1)
+#     if range_left.distance_mm <= 100:
+#         movement.move_forward()
+#     elif range_left.distance_mm >= 100:
+#         movement.stop()
