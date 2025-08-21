aa7a633830edc07849813b7b70463b6f0afe4a78
Michael.L <Michael.liondis@education.nsw.gov.au>
2025-08-21 20:07:08 +1000

    whole lotta comments

--- project/lib/controller.py
+++ project/lib/controller.py
@@ -1,104 +1,105 @@
-from time import time
-from PiicoDev_Unified import sleep_ms
-from PiicoDev_SSD1306 import *
+from time import time                   # import time for tracking state changes
+from PiicoDev_Unified import sleep_ms   # sleep function in miliseconds 
+from PiicoDev_SSD1306 import *          # OLED display library
 
+# create OLED display object
 display = create_PiicoDev_SSD1306()
 
 class Controller: 
     def __init__(self, wheels, Lultra, Fultra, colour_sensor, debug):
+        # store the references to wheels, ultrsonic sensor, and colour sensor
         self.__wheels = wheels
-        self.__Lultra = Lultra
-        self.__Fultra = Fultra
-        self.__colour = colour_sensor
-        self.state = "IDLE"
+        self.__Lultra = Lultra          # left ultrasonic
+        self.__Fultra = Fultra          # front ultrasonic 
+        self.__colour = colour_sensor   # victim/colour sensor
+        self.state = "IDLE"             # initial state
         self.last_state_change = time()
         self.__debug = debug
 
-        self.turn_delay = 0.5
+        self.turn_delay = 0.5           # delay before switching out of turn
 
-    def idle_state(self): #     STOP state
+    def idle_state(self): #STOP state
         if self.__debug:
             print("System: IDLE state")
         self.state = "IDLE"
-        self.__wheels.stop()
+        self.__wheels.stop()                # stops wheels
         self.last_state_change = time()
 
     def set_move_forwards_state(self):
         if self.__debug:
             print("System: FORWARDS state")
         self.state = "FORWARDS"
-        self.__wheels.move_forward()
+        self.__wheels.move_forward()        # drives forward 
         self.last_state_change = time()
 
     def set_move_backwards_state(self):
         if self.__debug:
             print("System: BACKWARDS state")
         self.state = "BACKWARDS"
-        self.__wheels.move_backward()
+        self.__wheels.move_backward()       # drives backwards
         self.last_state_change = time()
 
     def set_rotate_right_state(self):
         if self.__debug:
             print("System: RIGHT state")
         self.state = "RIGHT"
-        self.__wheels.rotate_right()
+        self.__wheels.rotate_right()        # turns right
         self.last_state_change = time()
 
     def set_rotate_left_state(self):
         if self.__debug:
             print("System: LEFT state")
         self.state = "LEFT"
-        self.__wheels.rotate_left()
+        self.__wheels.rotate_left()         # turns left
         self.last_state_change = time()
 
     def error_state(self):
         if self.__debug:
             print("System: ERROR state")
         self.state = "ERROR"
-        self.__wheels.stop() 
+        self.__wheels.stop()                # stop robot on an error
         self.last_state_change = time()
 
     def update(self):
         now = time()
 
-        # wait for delay to be over before anything after move forwards
+        # wait for delay to be over before doing anything after turning or going bakcwards
         if self.state in ("LEFT", "RIGHT", "BACKWARDS"):
             if now - self.last_state_change < self.turn_delay:
                 return # continues the turn
             else:
-                self.__wheels.move_forward()
+                self.__wheels.move_forward()    # resumes forward motion
         
-        # when senses victim, stops for 5 seconds then continues
+        # victim sensing logic
         VictimStatus = self.__colour.SenseVictim()
 
         if VictimStatus == "green":
-            self.idle_state()
-            sleep_ms(4000)
-            self.__wheels.move_forward()
-            sleep_ms(500)
+            self.idle_state()               # stops robot
+            sleep_ms(4000)                  # idle for 4000 miliseconds
+            self.__wheels.move_forward()    # moves forward
+            sleep_ms(500)                   # small forward movement to make sure it doesnt sense the same victim/colour again
         if VictimStatus == "not green":
-            display.show()
-            display.fill(0)
+            display.show()                  # refresh the display
+            display.fill(0)                 # clear the display
         
+        # read distances from ultrasonic sensors
         front_dist = self.__Fultra.distance_mm
         left_dist = self.__Lultra.distance_mm
 
-        wall_dist = 100 
+        wall_dist = 100     # distance to the wall that the ultras will read
 
-        # wall to the left and wall to the right 
+        # Case 1: wall in fron AND on left --> turn right (so left ultra is facing wall)
         if front_dist < wall_dist and left_dist < wall_dist:
-            # turn right
-            self.idle_state()
-            sleep_ms(2000)
+            self.idle_state()             
+            sleep_ms(2000)              
             self.set_rotate_right_state()
             sleep_ms(1050)
             self.idle_state()
             sleep_ms(2000)
             self.last_state_change = now
 
-        # wall in front    
+        # Case 2: Wall in front --> turn right (so left ultra is facing wall)
         elif front_dist < wall_dist:
-            #turn right so ultra is facing wall
             self.idle_state()
             sleep_ms(2000)
             self.set_rotate_right_state()
@@ -107,7 +108,7 @@ class Controller:
             sleep_ms(2000)
             self.last_state_change = now
         
-        # when theres no wall on the left and front
+        # Case 3: No wall on left AND no wall in front --> turn left 
         elif left_dist > wall_dist:
             # turn left 
             sleep_ms(500)
@@ -119,45 +120,6 @@ class Controller:
             sleep_ms(2000)
             self.last_state_change = now
         
+        # Case 4: Default --> moves forwards
         else:
             self.set_move_forwards_state()
-
-
-
-
-# from movement import Movement
-# from victim_sensor import Victim_Sensor
-# from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
-# from PiicoDev_VEML6040 import PiicoDev_VEML6040
-# from PiicoDev_SSD1306 import *
-# from servo import Servo
-# from machine import Pin, PWM
-# from time import sleep
-
-# range_front = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-# range_left = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
-
-# sensor = PiicoDev_VEML6040()
-# display = create_PiicoDev_SSD1306()
-
-# Victim_Sensor = Victim_Sensor(display, sensor, False)
-
-# freq = 50
-# min_us = 500
-# max_us = 2500
-# dead_zone_us = 1500
-
-# servo_pwm_left = PWM(Pin(16))
-# servo_pwm_right = PWM(Pin(15))
-
-# left_servo = Servo(pwm=servo_pwm_left, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq)
-
-# right_servo = Servo(pwm=servo_pwm_right, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq)
-
-# movement = Movement(right_servo, left_servo, False)
-
-# system = Controller(movement, range_left, range_front, Victim_Sensor, True)
-
-# while True: 
-#     system.update()
-#     sleep(0.1)
--- project/lib/victim_sensor.py
+++ project/lib/victim_sensor.py
@@ -1,24 +1,31 @@
-from PiicoDev_SSD1306 import *
-from PiicoDev_Unified import sleep_ms
+from PiicoDev_SSD1306 import *          # import pico OLED display library
+from PiicoDev_Unified import sleep_ms   # import sleep function in miliseconds
 
+# create OLED display object
 display = create_PiicoDev_SSD1306()
 
 class Victim_Sensor:
     def __init__(self, display, colour_sensor, debug=False):
+        # instantiation of colour sensor and OLED screen
         self.__colour_sensor = colour_sensor
         self.__debug = debug
         self.__display = display
 
     def SenseVictim(self):
         if self.__debug:
-            print("sensing")
+            print("sensing")    # debug output when sensing starts 
+        
+        # read hsv values from colour sensor
         data = self.__colour_sensor.readHSV()
 
+        # extract hue value
         hue = data['hue']
+
+        # check if hue is within the green range 
         if hue > 75 and hue < 85:
-            display.text("Victim Sensed",1,20, 1)
-            display.show()
-            return "green"
+            display.text("Victim Sensed",1,20, 1)   # write message on display
+            display.show()                          # update display 
+            return "green"                          # return detection of colour
         else:
-            display.fill(0)
-            return "not green"
+            display.fill(0)                         # clear display if no victim
+            return "not green"                      # return no detection 
